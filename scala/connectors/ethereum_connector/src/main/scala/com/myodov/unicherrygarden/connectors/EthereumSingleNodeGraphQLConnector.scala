52333a9c2e31e16b7078b466c99fdb68a533ee12
Alex Myodov <amyodov@gmail.com>
2021-12-27 12:09:58 +0200

    Common GraphQL query handling.

--- scala/connectors/ethereum_connector/src/main/scala/com/myodov/unicherrygarden/connectors/EthereumSingleNodeGraphQLConnector.scala
+++ scala/connectors/ethereum_connector/src/main/scala/com/myodov/unicherrygarden/connectors/EthereumSingleNodeGraphQLConnector.scala
@@ -4,9 +4,9 @@ import java.time.Instant
 
 import akka.actor.typed.{ActorSystem => TypedActorSystem}
 import akka.actor.{ActorSystem => ClassicActorSystem}
-import caliban.client.CalibanClientError
+import caliban.client.Operations.RootQuery
+import caliban.client.{CalibanClientError, SelectionBuilder}
 import com.myodov.unicherrygarden.api.dlt
-import com.myodov.unicherrygarden.api.dlt.{EthereumBlock, EthereumMinedTransaction}
 import com.myodov.unicherrygarden.connectors.AbstractEthereumNodeConnector.{SingleBlockData, SyncingStatus}
 import com.myodov.unicherrygarden.connectors.graphql._
 import com.myodov.unicherrygarden.ethereum.EthUtils
@@ -14,7 +14,7 @@ import com.typesafe.scalalogging.LazyLogging
 import sttp.capabilities
 import sttp.capabilities.akka.AkkaStreams
 import sttp.client3.akkahttp.AkkaHttpBackend
-import sttp.client3.{Response, SttpBackend, UriContext}
+import sttp.client3.{Request, Response, SttpBackend, UriContext}
 import sttp.model.Uri
 
 import scala.annotation.switch
@@ -40,58 +40,67 @@ class EthereumSingleNodeGraphQLConnector(nodeUrl: String,
       case Some(actorSystem: ClassicActorSystem) => AkkaHttpBackend.usingActorSystem(actorSystem)
     }
 
-  override def ethSyncingBlockNumber: Option[SyncingStatus] = {
-    import caliban.Geth._
-
-    // Either we have some data in `syncing`; or we must get the most recent block as just `block {number hash}`
-
-    val query = Query.syncing {
-      SyncState.view
-    } ~ Query.block() {
-      BlockMinimal.view
-    }
-
-    val rq = query.toRequest(graphQLUri)
-
+  /** Perform a GraphQL query with all the necessary error handling.
+   *
+   * `QV` - either `(Option[SyncState.SyncStateView], Option[BlockMinimalView])` or List[BlockBasicView]`,
+   * or something similar..
+   * */
+  private[this] def queryGraphQL[QV](query: SelectionBuilder[RootQuery, QV],
+                                     argHint: String): Option[QV] = {
+    val rq: Request[Either[CalibanClientError, QV], Any] = query.toRequest(graphQLUri)
     try {
       val value = Await.result(rq.send(sttpBackend), AbstractEthereumNodeConnector.NETWORK_TIMEOUT)
 
       value.body match {
         case Left(err) =>
-          logger.error(s"Error for GraphQL querying sync state", err)
+          logger.error(s"Error for GraphQL querying $argHint", err)
           None
-        case Right((optSyncing, optRecentBlock)) =>
-          // Received a valid response; do something with both paths:
-          (optSyncing, optRecentBlock) match {
-            case (Some(syncing), _) =>
-              // The node is still syncing
-              logger.debug(s"The node is still syncing: $syncing")
-              Some(SyncingStatus(
-                currentBlock = Math.toIntExact(syncing.currentBlock),
-                highestBlock = Math.toIntExact(syncing.highestBlock)
-              ))
-            case (None, Some(latestBlock)) =>
-              logger.debug(s"The node is fully synced, most recent block is $latestBlock")
-              // Not syncing already; but does the block number really makes sense?
-              if (latestBlock.number > 0) // This is a sensible block
-                Some(SyncingStatus(
-                  currentBlock = Math.toIntExact(latestBlock.number),
-                  highestBlock = Math.toIntExact(latestBlock.number)
-                ))
-              else None // not really synced
-            case other =>
-              logger.debug(s"The node is in incomprehensible state of syncing: $optSyncing, $optRecentBlock")
-              None
-          }
+        case Right(res: QV) =>
+          // Received a valid response
+          Some(res)
         case other =>
-          logger.error(s"Unhandled GraphQL response for GraphQL querying sync state: $other")
+          logger.error(s"Unhandled GraphQL response for GraphQL querying $argHint: $other")
           None
       }
     } catch {
       case NonFatal(e) =>
-        logger.error(s"Some nonfatal error happened during GraphQL querying sync state", e)
+        logger.error(s"Some nonfatal error happened during GraphQL querying $argHint", e)
         None
     }
+  }
+
+  override def ethSyncingBlockNumber: Option[SyncingStatus] = {
+    import caliban.Geth._
+
+    // Either we have some data in `syncing`; or we must get the most recent block as just `block {number hash}`
+    val query: SelectionBuilder[RootQuery, (Option[SyncState.SyncStateView], Option[BlockMinimalView])] = Query.syncing {
+      SyncState.view
+    } ~ Query.block() {
+      BlockMinimal.view
+    }
+
+    // Received a valid response; do something with both paths:
+    queryGraphQL(query, argHint = "sync state").flatMap(_ match {
+      case (Some(syncing), _) =>
+        // The node is still syncing
+        logger.debug(s"The node is still syncing: $syncing")
+        Some(SyncingStatus(
+          currentBlock = Math.toIntExact(syncing.currentBlock),
+          highestBlock = Math.toIntExact(syncing.highestBlock)
+        ))
+      case (None, Some(latestBlock)) =>
+        logger.debug(s"The node is fully synced, most recent block is $latestBlock")
+        // Not syncing already; but does the block number really makes sense?
+        if (latestBlock.number > 0) // This is a sensible block
+          Some(SyncingStatus(
+            currentBlock = Math.toIntExact(latestBlock.number),
+            highestBlock = Math.toIntExact(latestBlock.number)
+          ))
+        else None // not really synced
+      case other =>
+        logger.debug(s"The node is in incomprehensible state of syncing: $other")
+        None
+    })
   }
 
   override def readBlock(blockNumber: BigInt): Option[SingleBlockData] = {
@@ -99,7 +108,8 @@ class EthereumSingleNodeGraphQLConnector(nodeUrl: String,
     // `readBlocks` is implemented in details, `readBlock` just calls it.
 
     // Option of Seq can be easily converted to a single Option with just flatMap
-    readBlocks(blockNumber.intValue to blockNumber.intValue).flatMap { _ match {
+    readBlocks(blockNumber.intValue to blockNumber.intValue).flatMap {
+      _ match {
         case emptySeq if emptySeq.isEmpty => None
         case Seq(singleItem) => Some(singleItem)
         case tooLargeSeq => None
@@ -107,135 +117,115 @@ class EthereumSingleNodeGraphQLConnector(nodeUrl: String,
     }
   }
 
-  override def readBlocks(range: EthereumBlock.BlockNumberRange): Option[Seq[SingleBlockData]] = {
+  override def readBlocks(range: dlt.EthereumBlock.BlockNumberRange): Option[Seq[SingleBlockData]] = {
     require(range.head <= range.end, range)
 
     import caliban.Geth._
 
-    val query = Query.blocks(from = Some(range.head), to = Some(range.end)) {
+    val query: SelectionBuilder[RootQuery, List[BlockBasicView]] = Query.blocks(from = Some(range.head), to = Some(range.end)) {
       BlockBasic.view
     }
 
-    val rq = query.toRequest(graphQLUri)
-
-    val result = try {
-      val value: Response[Either[CalibanClientError, List[BlockBasicView]]] =
-        Await.result(rq.send(sttpBackend), AbstractEthereumNodeConnector.NETWORK_TIMEOUT)
-
-      value.body match {
-        case Left(err) =>
-          logger.error(s"Error for GraphQL querying blocks $range", err)
-          None
-        case Right(optBlocksBasic: Seq[BlockBasicView]) =>
-          // This is a legit response; but it may have no contents.
-          // For None, return None; for Some return a result,... hey it’s a map!
-          val seq = optBlocksBasic.map { blockBasic =>
-            // Validate block
-            {
-              // Different validations depending on whether parent is Some(block) or None:
-              // “parent is absent” may happen only on the block 0;
-              // “parent is not absent” implies the parent block has number lower by one.
-              require(blockBasic.parent match {
-                case None => blockBasic.number == 0
-                case Some(parentBlock) => parentBlock.number == blockBasic.number - 1
-              },
-                blockBasic)
-              require(
-                blockBasic.transactions match {
-                  // If the transactions are not available at all – that’s legit
-                  case None => true
-                  // If the transactions are available - all of them must refer to the same block
-                  case Some(trs) => trs.forall { tr =>
-                    // Inner block must refer to the outer block
-                    (tr.block match {
-                      case Some(innerBlock) => innerBlock == blockBasic.asMinimalBlock
-                      case None => false
-                    }) &&
-                      // All inner logs must refer to the outer transaction
-                      (tr.logs match {
-                        // If there are no logs at all, that’s okay
-                        case None => true
-                        // But if there are some logs, all of them must refer to the same transaction
-                        case Some(logs) => logs.forall(_.transaction == tr.asMinimalTransaction)
-                      })
-                  }
-                },
-                blockBasic
-              )
-            }
-
-            val blockNumber = Math.toIntExact(blockBasic.number)
-
-            val block = dlt.EthereumBlock(
-              number = blockNumber,
-              hash = blockBasic.hash,
-              parentHash = blockBasic.parent match {
-                // We need some custom handling of parent
-                // to make it compatible with RPC/block explorers
-                case None => Some("0x0000000000000000000000000000000000000000000000000000000000000000")
-                case Some(parent) => Some(parent.hash)
-              },
-              timestamp = Instant.ofEpochSecond(blockBasic.timestamp)
-            )
-            val transactions = blockBasic.transactions match {
-              case None => Seq()
-              case Some(transactions) => transactions.map { (tr: TransactionFullView) =>
-                dlt.EthereumMinedTransaction(
-                  // *** Before-mined transaction ***
-                  txhash = tr.hash,
-                  from = tr.from.address,
-                  to = tr.to.map(_.address), // Option(nullable)
-                  gas = tr.gas,
-                  gasPrice = tr.gasPrice,
-                  nonce = Math.toIntExact(tr.nonce),
-                  value = tr.value,
-                  // *** Mined transaction ***
-                  // "status" – EIP 658, since Byzantium fork
-                  // using Option(nullable).
-                  // But there seems to be a bug in GraphQL handling pre-Byzantium statuses,
-                  // (https://github.com/ethereum/go-ethereum/issues/24124)
-                  // So need to handle this manually.
-                  status = blockNumber match {
-                    case preByzantium if preByzantium < EthUtils.BYZANTIUM_FIRST_BLOCK =>
-                      None
-                    case byzantiumAndNewer =>
-                      tr.status.map(Math.toIntExact) // Option[Long] to Option[Int]
-                  },
-                  blockNumber = tr.block.get.number, // block must exist!
-                  transactionIndex = tr.index.get, // transaction must exist!
-                  gasUsed = tr.gasUsed.get, // presumed non-null if mined
-                  effectiveGasPrice = tr.effectiveGasPrice.get, // presumed non-null if mined
-                  cumulativeGasUsed = tr.cumulativeGasUsed.get, // presumed non-null if mined
-                  txLogs = tr.logs match {
-                    case None => Seq.empty
-                    case Some(logs) => logs.map { log =>
-                      dlt.EthereumTxLog(
-                        logIndex = log.index,
-                        address = log.account.address,
-                        topics = log.topics,
-                        data = log.data
-                      )
-                    }
-                  }
-                )
+    // This is a legit response; but it may have no contents.
+    // For None, return None; for Some return a result,... hey it’s a map!
+    queryGraphQL(query, argHint = s"blocks $range").map { // Option of Seq of Blocks
+      _.map { blockBasic =>
+        // Validate block
+        {
+          // Different validations depending on whether parent is Some(block) or None:
+          // “parent is absent” may happen only on the block 0;
+          // “parent is not absent” implies the parent block has number lower by one.
+          require(blockBasic.parent match {
+            case None => blockBasic.number == 0
+            case Some(parentBlock) => parentBlock.number == blockBasic.number - 1
+          },
+            blockBasic)
+          require(
+            blockBasic.transactions match {
+              // If the transactions are not available at all – that’s legit
+              case None => true
+              // If the transactions are available - all of them must refer to the same block
+              case Some(trs) => trs.forall { tr =>
+                // Inner block must refer to the outer block
+                (tr.block match {
+                  case Some(innerBlock) => innerBlock == blockBasic.asMinimalBlock
+                  case None => false
+                }) &&
+                  // All inner logs must refer to the outer transaction
+                  (tr.logs match {
+                    // If there are no logs at all, that’s okay
+                    case None => true
+                    // But if there are some logs, all of them must refer to the same transaction
+                    case Some(logs) => logs.forall(_.transaction == tr.asMinimalTransaction)
+                  })
               }
-            }
-            (block, transactions)
+            },
+            blockBasic
+          )
+        }
+
+        val blockNumber = Math.toIntExact(blockBasic.number)
+
+        val block = dlt.EthereumBlock(
+          number = blockNumber,
+          hash = blockBasic.hash,
+          parentHash = blockBasic.parent match {
+            // We need some custom handling of parent
+            // to make it compatible with RPC/block explorers
+            case None => Some("0x0000000000000000000000000000000000000000000000000000000000000000")
+            case Some(parent) => Some(parent.hash)
+          },
+          timestamp = Instant.ofEpochSecond(blockBasic.timestamp)
+        )
+        val transactions = blockBasic.transactions match {
+          case None => Seq()
+          case Some(transactions) => transactions.map { (tr: TransactionFullView) =>
+            dlt.EthereumMinedTransaction(
+              // *** Before-mined transaction ***
+              txhash = tr.hash,
+              from = tr.from.address,
+              to = tr.to.map(_.address), // Option(nullable)
+              gas = tr.gas,
+              gasPrice = tr.gasPrice,
+              nonce = Math.toIntExact(tr.nonce),
+              value = tr.value,
+              // *** Mined transaction ***
+              // "status" – EIP 658, since Byzantium fork
+              // using Option(nullable).
+              // But there seems to be a bug in GraphQL handling pre-Byzantium statuses,
+              // (https://github.com/ethereum/go-ethereum/issues/24124)
+              // So need to handle this manually.
+              status = blockNumber match {
+                case preByzantium if preByzantium < EthUtils.BYZANTIUM_FIRST_BLOCK =>
+                  None
+                case byzantiumAndNewer =>
+                  tr.status.map(Math.toIntExact) // Option[Long] to Option[Int]
+              },
+              blockNumber = tr.block.get.number, // block must exist!
+              transactionIndex = tr.index.get, // transaction must exist!
+              gasUsed = tr.gasUsed.get, // presumed non-null if mined
+              effectiveGasPrice = tr.effectiveGasPrice.get, // presumed non-null if mined
+              cumulativeGasUsed = tr.cumulativeGasUsed.get, // presumed non-null if mined
+              txLogs = tr.logs match {
+                case None => Seq.empty
+                case Some(logs) => logs.map { log =>
+                  dlt.EthereumTxLog(
+                    logIndex = log.index,
+                    address = log.account.address,
+                    topics = log.topics,
+                    data = log.data
+                  )
+                }
+              }
+            )
           }
-          Some(seq)
-        case other =>
-          logger.error(s"Unhandled GraphQL response for blocks $range: $other")
-          None
+        }
+        (block, transactions)
       }
-    } catch {
-      case NonFatal(e) =>
-        logger.error(s"Some nonfatal error happened during GraphQL query for $range", e)
-        None
     }
-    result
   }
 
-  override def readBlockHashes(range: EthereumBlock.BlockNumberRange): Option[SortedMap[Int, String]] = {
+  override def readBlockHashes(range: dlt.EthereumBlock.BlockNumberRange): Option[SortedMap[Int, String]] = {
     require(range.head <= range.end, range)
 
     import caliban.Geth._
