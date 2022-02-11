2d8f9ded93d63e97e6eb1cc66cee12595cea8346
Alex Myodov <amyodov@gmail.com>
2022-02-11 18:17:23 +0200

    Connector creation changes: now support providing the UniCherryGarden realm (to allow, in the future, several UniCherryGarden systems to co-exist in the same Akka cluster) and Ethereum network Chain ID (to explicitly specify EIP-55 Chain ID to be able to create the transactions in any chain).

--- java/cherrygardener_connector_cli/src/main/java/com/myodov/unicherrygarden/cherrygardener/ConfFile.java
+++ java/cherrygardener_connector_cli/src/main/java/com/myodov/unicherrygarden/cherrygardener/ConfFile.java
@@ -4,13 +4,14 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigException;
 import com.typesafe.config.ConfigFactory;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * The API to the CLI conf file access.
@@ -18,8 +19,6 @@ import java.util.Optional;
  * The file should be a HOCON conf file, and located in <code>~/.unicherrygarden/cli.conf</code>.
  */
 public class ConfFile {
-    private static final Logger logger = LoggerFactory.getLogger(ConfFile.class);
-
     private final Path confPath = Paths.get(
             System.getProperty("user.home"),
             ".unicherrygarden",
@@ -34,46 +33,110 @@ public class ConfFile {
     }
 
     /**
-     * Note: prints errors to STDERR.
+     * @param cliParamName the name of parameter, as can be given in CLI (e.g. "--connect").
+     * @param configPath   the path to the parameter in HOCON file (e.g. "connect.urls").
+     * @param validator    (optional) validator that tests the candidate before returning;
+     *                     if validation fails, it is the validator’s own duty to print the explanation to STDERR,
+     *                     if needed.
+     * @implNote prints errors to STDERR.
      */
     @NonNull
-    public Optional<List<String>> getConnectUrls() {
-        final @NonNull List<String> candidate;
+    public final <RES> Optional<RES> getParamFromCliOrConfig(
+            @NonNull String cliParamName,
+            @NonNull String configPath,
+            @NonNull Function<String, RES> getter,
+            @Nullable Predicate<RES> validator
+    ) {
+        final @NonNull RES candidate;
         try {
-            candidate = config.getStringList("connect.urls");
+            candidate = getter.apply(configPath);
+
+            if (validator != null && !validator.test(candidate)) {
+                return Optional.empty();
+            }
         } catch (ConfigException.Missing e) {
             // No setting but this is okay, no error
-            System.err.printf("Note: instead of --connect CLI argument, you can put \"connect.urls\" setting in %s HOCON conf file.\n", confPath);
+            System.err.printf("Note: instead of %s CLI argument, you can put \"%s\" setting in %s HOCON conf file.\n",
+                    cliParamName, configPath, confPath);
             return Optional.empty();
         } catch (ConfigException.WrongType e) {
-            System.err.println("`connect.urls` setting in your conf file is of wrong type!");
+            System.err.printf("`%s` setting in your conf file is of wrong type!\n",
+                    configPath);
             return Optional.empty();
         }
 
         return Optional.of(candidate);
     }
 
+
     /**
-     * Note: prints errors to STDERR.
+     * @implNote prints errors to STDERR.
      */
     @NonNull
-    public Optional<Integer> getListenPort() {
-        final int candidate;
-        try {
-            candidate = config.getInt("listen.port");
-            if (!(0 <= candidate && candidate <= 65535)) {
-                System.err.println("\"listen.port\" setting in your conf file must be a port in [0..65535] range!");
-                return Optional.empty();
-            }
-        } catch (ConfigException.Missing e) {
-            // No setting but this is okay, no error
-            System.err.printf("Note: instead of --listen-port CLI argument, you can put \"listen.port\" setting in %s HOCON conf file.\n", confPath);
-            return Optional.empty();
-        } catch (ConfigException.WrongType e) {
-            System.err.println("`listen.port` setting in your conf file is of wrong type!");
-            return Optional.empty();
-        }
+    public final Optional<List<String>> getConnectUrls() {
+        return getParamFromCliOrConfig(
+                "--connect",
+                "connect.urls",
+                config::getStringList,
+                null
+        );
+    }
 
-        return Optional.of(candidate);
+    /**
+     * @implNote prints errors to STDERR.
+     */
+    @NonNull
+    public final Optional<Integer> getListenPort() {
+        return getParamFromCliOrConfig(
+                "--listen-port",
+                "listen.port",
+                config::getInt,
+                (candidate) -> {
+                    if (!(0 <= candidate && candidate <= 65535)) {
+                        System.err.println("\"listen.port\" setting in your conf file must be a port in [0..65535] range!");
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+        );
+    }
+
+    /**
+     * @implNote prints errors to STDERR.
+     */
+    @NonNull
+    public final Optional<String> getRealm() {
+        return getParamFromCliOrConfig(
+                "--realm",
+                "connect.realm",
+                config::getString,
+                null
+        );
+    }
+
+    /**
+     * @implNote prints errors to STDERR.
+     */
+    @NonNull
+    public final Optional<Long> getChainId() {
+        return getParamFromCliOrConfig(
+                "--chain-id",
+                "blockchain.chain_id",
+                config::getLong,
+                (candidate) -> {
+                    if (candidate == -1) {
+                        System.err.println("Chain ID value of -1 means None, but you probably want a real Chain ID " +
+                                "for EIP-155 compatibility. But using it for now.");
+                        return true;
+                    } else if (candidate < 1) {
+                        System.err.println("Chain ID value must be >=1; may be -1 but that means None " +
+                                "and is not recommended.");
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+        );
     }
 }
--- java/cherrygardener_connector_cli/src/main/java/com/myodov/unicherrygarden/cherrygardener/ConnectionSettings.java
+++ java/cherrygardener_connector_cli/src/main/java/com/myodov/unicherrygarden/cherrygardener/ConnectionSettings.java
@@ -1,5 +1,7 @@
 package com.myodov.unicherrygarden.cherrygardener;
 
+import com.myodov.unicherrygarden.connector.api.ClientConnector;
+import com.myodov.unicherrygarden.connector.impl.ClientConnectorImpl;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.List;
@@ -14,20 +16,43 @@ class ConnectionSettings {
     @NonNull
     public final List<String> connectUrls;
 
+    @NonNull
+    public final String realm;
+
+    public final long chainId;
+
+
     /**
      * Constructor.
      */
-    ConnectionSettings(int listenPort, @NonNull List<String> connectUrls) {
+    ConnectionSettings(int listenPort,
+                       @NonNull List<String> connectUrls,
+                       @NonNull String realm,
+                       long chainId) {
         assert 0 <= listenPort && listenPort <= 65535 : listenPort;
         assert connectUrls != null : connectUrls;
+        assert realm != null : realm;
+        assert chainId == -1 || chainId >= 1: chainId;
 
         this.listenPort = listenPort;
         this.connectUrls = connectUrls;
+        this.realm = realm;
+        this.chainId = chainId;
     }
 
     @Override
     public String toString() {
-        return String.format("ConnectionSettings(listenPort=%d, connectUrls=%s)",
-                listenPort, connectUrls);
+        return String.format("ConnectionSettings(listenPort=%d, connectUrls=%s, realm=%s, chainId=%s)",
+                listenPort, connectUrls, realm, chainId);
+    }
+
+    @NonNull
+    public final ClientConnector createClientConnector(int mandatoryConfirmations) {
+        return new ClientConnectorImpl(connectUrls, realm, chainId, listenPort, mandatoryConfirmations);
+    }
+
+    @NonNull
+    public final ClientConnector createClientConnector() {
+        return new ClientConnectorImpl(connectUrls, realm, chainId, listenPort);
     }
 }
