aca6211f5ba2b68603e01713996ce4b9e779e139
Alex Myodov <amyodov@gmail.com>
2022-02-13 14:52:08 +0200

    Simplifying the existing `ucg_get_currencies_for_keys_filter` function.

--- scala/storages/db_postgresql_storage/src/main/resources/com/myodov/unicherrygarden/db/migrations/repeatable/functions/R__create_function_ucg_get_currencies_for_keys_filter.sql
+++ scala/storages/db_postgresql_storage/src/main/resources/com/myodov/unicherrygarden/db/migrations/repeatable/functions/R__create_function_ucg_get_currencies_for_keys_filter.sql
@@ -8,26 +8,15 @@ CREATE OR REPLACE FUNCTION ucg_get_currencies_for_keys_filter(_has_filter_curren
     STABLE
 AS
 $$
-WITH
-    vars AS (
-        SELECT array_position(_filter_currency_keys, '') IS NOT NULL AS filter_contains_eth
-    )
 SELECT id AS currency_id
-FROM
-    vars,
-    ucg_currency
+FROM ucg_currency
 WHERE
     -- let's calculate a condition which matches the currencies
     CASE _has_filter_currency_keys
-        -- when we have filter - match the filter. Differently for ERC20 and ETH, though.
+        -- when we have filter - match the filter
         WHEN TRUE
-            -- two subconditions: either dapp address matches; or ETH matches
             THEN
-                (dapp_address IN (
-                    SELECT *
-                    FROM unnest(_filter_currency_keys)
-                )) OR
-                (filter_contains_eth AND ucg_currency.type = 'ETH')
+                ucg_get_currency_code(ucg_currency.type, ucg_currency.dapp_address) = ANY (_filter_currency_keys)
         ELSE -- when no filter, just not extra condition to filter
             TRUE
     END
