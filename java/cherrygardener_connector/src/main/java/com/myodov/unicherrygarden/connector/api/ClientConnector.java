602c74ee9c50f1c5a608a3bcfeb41c71cecfe3ff
Alex Myodov <amyodov@gmail.com>
2022-02-14 14:31:50 +0200

    More convenient API, to get just a single currency by its key.

--- java/cherrygardener_connector/src/main/java/com/myodov/unicherrygarden/connector/api/ClientConnector.java
+++ java/cherrygardener_connector/src/main/java/com/myodov/unicherrygarden/connector/api/ClientConnector.java
@@ -1,9 +1,11 @@
 package com.myodov.unicherrygarden.connector.api;
 
+import com.myodov.unicherrygarden.connector.impl.Validators;
 import com.myodov.unicherrygarden.messages.cherrygardener.GetCurrencies;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -19,8 +21,10 @@ public interface ClientConnector {
 
     /**
      * Get all the supported currencies, or some subset of them.
-     * Depending on the settings, may include verified, unverified currencies, or both; also may select some currencies
-     * by their keys, or not
+     * Depending on the settings, may include verified, unverified currencies, or both;
+     * also may select some currencies by their keys, or select all the currencies possible.
+     * <p>
+     * If you want just a single currency, use the similar {@link #getCurrency(String, boolean, boolean)}.
      *
      * @param filterCurrencyKeys if present, contains the currency keys that we are looking for;
      *                           if absent, all currencies should be retrieved.
@@ -35,13 +39,51 @@ public interface ClientConnector {
 
     /**
      * Get all the supported currencies. Only the currencies pre-validated (“verified”) are returned.
+     * <p>
+     * If you want just a single currency, use the similar {@link #getCurrency(String)}.
      *
      * @return the response with all the supported currencies.
-     * @throws IllegalArgumentException if neither `getVerified` nor `getUnverified` are defined.
      */
     @SuppressWarnings("unused")
     default GetCurrencies.@NonNull Response getCurrencies() {
         return getCurrencies(null, true, false);
+    }
+
+    /**
+     * Get a single supported currency, by its key.
+     * Depending on the settings, may include verified, unverified currencies, or both.
+     * <p>
+     * If you want multiple currencies at once, use the similar {@link #getCurrencies(Set, boolean, boolean)}.
+     *
+     * @param currencyKey a currency key
+     * @return the response with just this currency.
+     * @throws IllegalArgumentException if neither `getVerified` nor `getUnverified` are defined.
+     */
+    @SuppressWarnings("unused")
+    default GetCurrencies.@NonNull Response getCurrency(
+            @NonNull String currencyKey,
+            boolean getVerified,
+            boolean getUnverified) {
+        Validators.requireValidCurrencyKey("currencyKey", currencyKey);
+        return getCurrencies(
+                new HashSet<String>(1) {{
+                    add(currencyKey);
+                }},
+                getVerified,
+                getUnverified
+        );
+    }
+
+    /**
+     * Get a single supported currency, by its key. The currency is returned only if it is pre-validated (“verified”).
+     * <p>
+     * If you want multiple currencies at once, use the similar {@link #getCurrencies()}.
+     *
+     * @return the response with all the supported currencies.
+     */
+    @SuppressWarnings("unused")
+    default GetCurrencies.@NonNull Response getCurrency(@NonNull String currencyKey) {
+        return getCurrency(currencyKey, true, false);
     }
 
     /**
