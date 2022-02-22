a5b7029d441400626292eff1ff1ef5b939b00c5d
Alex Myodov <amyodov@gmail.com>
2022-02-22 20:05:06 +0200

    Distributing the blocking Akka operations to separate dispatchers - regular blocking and heavy-load blocking one.

--- scala/api/src/main/scala/com/myodov/unicherrygarden/EthereumStatePoller.scala
+++ scala/api/src/main/scala/com/myodov/unicherrygarden/EthereumStatePoller.scala
@@ -2,6 +2,7 @@ package com.myodov.unicherrygarden
 
 import akka.actor.typed.scaladsl.{Behaviors, TimerScheduler}
 import akka.actor.typed.{ActorRef, Behavior}
+import com.myodov.unicherrygarden.EthereumStatePoller.LogActorSystemStatus
 import com.myodov.unicherrygarden.api.GardenMessages
 import com.typesafe.scalalogging.LazyLogging
 
@@ -19,30 +20,39 @@ private class EthereumStatePoller(ethereumConnector: AbstractEthereumNodeConnect
   import EthereumStatePoller.{Message, Poll}
   import GardenMessages.EthereumNodeStatus
 
-  private def launch(listeners: Seq[ActorRef[GardenMessages.EthereumNodeStatus]]): Behavior[Message] = {
-    logger.debug(s"Launching Ethereum state poller for $listeners")
+  private def launch(listeners: Seq[ActorRef[GardenMessages.EthereumNodeStatus]]): Behavior[Message] =
+    Behaviors.setup { context =>
+      logger.debug(s"Launching Ethereum state poller for $listeners")
 
-    Behaviors.withTimers[Message] { timers: TimerScheduler[Message] =>
-      timers.startTimerWithFixedDelay(
-        Poll(),
-        0 seconds,
-        CherryGardenComponent.BLOCK_ITERATION_PERIOD,
-      )
-      // due to initialDelay = 0 seconds, it also sends this message, instantly, too.
-      Behaviors.receiveMessage[Message] {
-        case Poll() =>
-          logger.debug("Polling Ethereum node for syncing status...")
-          // If polling successful (and only then), resend the syncing status to listeners
-          ethereumConnector.ethBlockchainStatus.foreach { syncingStatus =>
-            val msg = EthereumNodeStatus(syncingStatus)
-            for (listener <- listeners) {
-              listener ! msg
+      Behaviors.withTimers[Message] { timers: TimerScheduler[Message] =>
+        timers.startTimerWithFixedDelay(
+          Poll(),
+          0 seconds,
+          CherryGardenComponent.BLOCK_ITERATION_PERIOD,
+        )
+        timers.startTimerWithFixedDelay(
+          LogActorSystemStatus(),
+          0 seconds,
+          1 minute,
+        )
+        // due to initialDelay = 0 seconds, it also sends this message, instantly, too.
+        Behaviors.receiveMessage[Message] {
+          case Poll() =>
+            logger.debug("Polling Ethereum node for syncing status...")
+            // If polling successful (and only then), resend the syncing status to listeners
+            ethereumConnector.ethBlockchainStatus.foreach { syncingStatus =>
+              val msg = EthereumNodeStatus(syncingStatus)
+              for (listener <- listeners) {
+                listener ! msg
+              }
             }
-          }
-          Behaviors.same
+            Behaviors.same
+          case LogActorSystemStatus() =>
+            logger.info(s"ActorSystem status: ${context.system}\n${context.system.printTree}")
+            Behaviors.same
+        }
       }
     }
-  }
 }
 
 /** A simple helper to poll the Ethereum */
@@ -53,6 +63,9 @@ object EthereumStatePoller {
 
   /** The message to run the next polling attempt. */
   private final case class Poll() extends Message
+
+  /** The message to log the actor system status. */
+  private final case class LogActorSystemStatus() extends Message
 
   /** Main constructor. */
   @inline def apply(ethereumConnector: AbstractEthereumNodeConnector with Web3ReadOperations,
